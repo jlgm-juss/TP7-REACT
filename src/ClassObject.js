0caf7ca4ee3f55cb0f93b4267adaca5e7fa5e3eb
JLGM <juss.jlgm@gmail.com>
2022-10-03 23:37:16 -0300

    se agrego class

---
+++ src/ClassObject.js
@@ -0,0 +1,9 @@
+export default class prototypeEmpleado{
+    constructor(id,fullName,title,departament,pic){
+        this.id=id;
+        this.fullName=fullName;
+        this.title=title;
+        this.departament=departament;
+        this.pic=pic;
+    }
+};
